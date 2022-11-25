2315c060e53b0cf61199df067e7e3b803f2f32e8
Revoxelizer <87579978+Revoxelizer@users.noreply.github.com>
2022-11-24 19:32:44 -0800

    Create PlayerControllerX.cs

---
+++ CwC Challenge 1/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 20;
+    public float rotationSpeed = 1;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * -90 * Time.deltaTime * verticalInput);
+    }
+}
